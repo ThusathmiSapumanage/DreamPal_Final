9b893d1660fd252d46f2545d0c262a0305d74ac1
Thusathmi Sapumanage <169069405+ThusathmiSapumanage@users.noreply.github.com>
2025-05-13 20:55:06 +0530

    initial commit

---
+++ DreamPal-Back/DreamPal-Auth/DreamPal-Auth/src/main/java/com/example/DreamPal_Auth/DreamPalAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.DreamPal_Auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamPalAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamPalAuthApplication.class, args);
+	}
+
+}
