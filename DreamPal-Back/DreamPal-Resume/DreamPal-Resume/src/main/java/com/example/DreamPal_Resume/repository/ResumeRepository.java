10951beb43cebe1d62b313fe3dbe7b8a4f0488d4
Thusathmi Sapumanage <169069405+ThusathmiSapumanage@users.noreply.github.com>
2025-05-15 23:37:08 +0530

    done

--- DreamPal-Back/DreamPal-Resume/DreamPal-Resume/src/main/java/com/example/DreamPal_Resume/repository/ResumeRepository.java
+++ DreamPal-Back/DreamPal-Resume/DreamPal-Resume/src/main/java/com/example/DreamPal_Resume/repository/ResumeRepository.java
@@ -1,4 +1,10 @@
 package com.example.DreamPal_Resume.repository;
 
-public interface ResumeRepository {
+import com.example.DreamPal_Resume.entity.Resume;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface ResumeRepository extends MongoRepository<Resume, String> {
+    Optional<Resume> findByUserId(String userId);
 }
